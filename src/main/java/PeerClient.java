6c5593070b94a92b88643ca653d2055961d700a8
Lucas Dantas <lucasdantas08@gmail.com>
2023-06-26 14:05:01 -0300

    Consertando Bugs

--- src/main/java/PeerClient.java
+++ src/main/java/PeerClient.java
@@ -20,7 +20,7 @@ public class PeerClient {
     public static final String DEFAULT_IP = "127.0.0.1";
     public static final int DEFAULT_PORT = 1099;
 
-
+    // Construtor da Classe
     public PeerClient(String ip, int port, String folderPath, ArrayList<String> fileNames) {
         this.ip = ip;
         this.port = port;
@@ -28,15 +28,23 @@ public class PeerClient {
         this.fileNames = fileNames;
     }
 
+    // Main da Classe
     public static void main(String[] args) throws NotBoundException, RemoteException, ServerNotActiveException {
+        // Inicia uma instância do Peer para guardar informações
         PeerClient peerClient = PeerInicialization();
 
-        Registry registry = LocateRegistry.getRegistry(DEFAULT_IP, DEFAULT_PORT);
-        RequisitionInterface server = (RequisitionInterface) registry.lookup("server");
+        // Conecta ao serviço do servidor via RMI
 
-        menu(peerClient, server);
+        // Localiza o registro do servidor usando IP e porta padrão
+        Registry registry = LocateRegistry.getRegistry(DEFAULT_IP, DEFAULT_PORT);
+        // Conecta ao serviço server instanciado no servidor
+        RequisitionInterface requisitions = (RequisitionInterface) registry.lookup("requisitions");
+
+        // Inicia o menu interativo
+        menu(peerClient, requisitions);
     }
 
+    // Função para listar arquivos de uma pasta
     private static ArrayList<String> FileListing(String folderPath) {
         ArrayList<String> filesNames =  new ArrayList<>();
         File folder = new File(folderPath);
@@ -53,61 +61,74 @@ public class PeerClient {
         return filesNames;
     }
 
+    // Iniciador do peer
     private static PeerClient PeerInicialization() {
+        // Captura IP do Cliente
         Scanner sc = new Scanner(System.in);
         System.out.print("Favor inserir o IP do cliente:");
         String ip = sc.nextLine().trim();
 
+        // Captura porta do Cliente
         System.out.print("Favor inserir a porta do cliente:");
         String portString = sc.nextLine().trim();
         int port = parseInt(portString);
 
+        // Captura caminho da pasta em que os arquivos serão manipulados
         System.out.print("Favor inserir a pasta do cliente:");
         String folderPath = sc.nextLine().trim();
 
+        // Gera array com nomes de arquivos que estão dentro da pasta
         ArrayList<String> fileNames = FileListing(folderPath);
 
+        // Cria a instância do Peer com as informações capturadas
         return new PeerClient(ip, port, folderPath, fileNames);
     }
 
     private static void menu(PeerClient peerClient, RequisitionInterface server) throws ServerNotActiveException, RemoteException {
         Scanner sc = new Scanner(System.in);
-        int function;
+        String function;
 
+        // Instanciando Thread que gerenciará os downloads
         DownloadRequisitionReceiver drr = new DownloadRequisitionReceiver(peerClient);
 
         // Inicia thread para tratar requisições de download paralelamente
         Thread drrThread = new Thread(drr);
         drrThread.start();
 
+        // Loop para menu interativo
         do {
             System.out.println("---------Menu do Peer---------");
-            System.out.println("Qual a função que deseja usar?");
-            System.out.println("1 - JOIN");
-            System.out.println("2 - SEARCH");
-            System.out.println("3 - DOWNLOAD");
+            System.out.println("Qual a funcao que deseja usar?");
+            System.out.println("JOIN");
+            System.out.println("SEARCH");
+            System.out.println("DOWNLOAD");
             System.out.println("Aperte 0 para desligar o peer");
-            function = sc.nextInt();
+            function = sc.nextLine().trim();
 
             switch (function) {
-                case 1:
+                case "JOIN":
                     joinReq(peerClient, server);
                     break;
-                case 2:
+                case "SEARCH":
                     searchReq(peerClient, server);
                     break;
-                case 3:
+                case "DOWNLOAD":
                     downloadReq(peerClient, server);
                     break;
-                case 0:
+                case "0":
+                    // Interrompe thread que gerencia downloads no caso de o usuário desligar o peer no console
                     drrThread.interrupt();
+                    break;
             }
 
-        } while (function != 0);
+        } while (!function.equals("0"));
     }
 
+    // Função que implementa o JOIN do lado do peer
     private static void joinReq(PeerClient peerClient, RequisitionInterface server) throws ServerNotActiveException, RemoteException {
+        // Envia requisição de JOIN via RMI
         String response = server.join(peerClient.getIp(), peerClient.getPort(), peerClient.getFileNames());
+        // Se receber JOIN_OK, escreve mensagem padrão de apresentação, caso contrário mostra mensagem de erro
         if (response.equals("JOIN_OK")) {
             System.out.println("Sou peer "
                     + peerClient.getIp()
@@ -116,40 +137,54 @@ public class PeerClient {
                     + " com arquivos "
                     + String.join(" ", peerClient.getFileNames()));
         } else {
-            System.out.println("Requisição falhou: " + response);
+            System.out.println("Requisicao falhou: " + response);
         }
     }
 
+    // Função que implementa o SEARCH do lado do peer
     private static void searchReq(PeerClient peerClient, RequisitionInterface server) throws ServerNotActiveException, RemoteException {
+        // Captura arquivo a ser procurado
         Scanner scanner = new Scanner(System.in);
         System.out.print("Informe o arquivo que deseja procurar: ");
         String fileName = scanner.nextLine().trim();
 
+        // Envia requisição SEARCH via RMI e recebe lista de peer com o arquivo
         ArrayList<String> response = server.search(peerClient.getIp(), peerClient.getPort(), fileName);
 
+        // Mostra mensagem na tela com peers que possuem arquivo desejado
         System.out.println("peers com arquivo solicitado: " + String.join(" ", response));
+
+        // Muda arquivo requisitado para o arquivo procurado caso exista algum peer com o arquivo desejado
         if (!response.isEmpty()) peerClient.setRequestedFile(fileName);
     }
 
+    // Função que implementa a requisição de DOWNLOAD
     private static void downloadReq(PeerClient peerClient, RequisitionInterface server) {
         Scanner scanner = new Scanner(System.in);
+
+        // Pede para o usuário informar o arquivo caso não tenha sido setado anteriormente
+        if (peerClient.getRequestedFile() == null || peerClient.getRequestedFile().isEmpty()) {
+            System.out.print("Favor informar arquivo a ser baixado: ");
+            String requestedFile = scanner.nextLine().trim();
+            peerClient.setRequestedFile(requestedFile);
+        }
+
+        // Capturando IP e porta do peer ao qual será solicitado o DOWNLOAD
         System.out.print("Informe o IP do peer a solicitar download: ");
         String ip = scanner.nextLine().trim();
         System.out.print("Informe a porta do peer a solicitar download: ");
         String port = scanner.nextLine().trim();
 
-        if (peerClient.getRequestedFile() == null || peerClient.getRequestedFile().isEmpty()) {
-            System.out.print("Favor informar arquivo: ");
-            String requestedFile = scanner.nextLine().trim();
-            peerClient.setRequestedFile(requestedFile);
-        }
-
+        // Instanciando thread que realizará o download
         DownloadHandler drs = new DownloadHandler(peerClient, ip, port, server);
+
+        // Inicia thread que realizará o download
         Thread drsThread = new Thread(drs);
         drsThread.start();
-
     }
 
+
+    // Thread que gerencia requisições de DOWNLOAD enviadas por outros peers
     static class DownloadRequisitionReceiver implements Runnable {
         private final PeerClient client;
 
@@ -159,12 +194,14 @@ public class PeerClient {
 
         @Override
         public void run() {
+            // Iniciando socket de servidor
             try (ServerSocket serverSocket = new ServerSocket(client.getPort())) {
 
                 do {
-
+                    // Iniciando Listener para receber TCP
                     Socket socket = serverSocket.accept();
 
+                    // Após receber a comunicação TCP, instancia thread para enviar arquivo paralelamente
                     DownloadSender ds = new DownloadSender(socket, client);
 
                     // Inicia thread para enviar arquivo paralelamente
@@ -178,6 +215,7 @@ public class PeerClient {
         }
     }
 
+    // Thread que gerencia o DOWNLOAD
     static class DownloadHandler implements Runnable {
         private static final int BUFFER_SIZE = 4096;
         private final PeerClient client;
@@ -195,76 +233,84 @@ public class PeerClient {
         @Override
         public void run() {
             try {
+                // Iniciando conexão TCP com outro peer
                 Socket socket = new Socket(ip, port);
 
+                // Envia nome de arquivo desejado via TCP
                 OutputStream outputStream = socket.getOutputStream();
                 DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
                 dataOutputStream.writeUTF(client.getRequestedFile());
 
+                // Recebe resposta do outro peer
                 InputStream inputStream = socket.getInputStream();
                 DataInputStream dataInputStream = new DataInputStream(inputStream);
                 String downloadResponse = dataInputStream.readUTF();
 
+                // Verificando resposta do outro peer
                 if (downloadResponse.equals("FILE_NOT_FOUND")) {
+                    // Fecha socket e streams caso o peer informe que não encontrou o arquivo
+                    System.out.println("Arquivo não encontrado no peer informado");
                     outputStream.close();
                     dataOutputStream.close();
                     inputStream.close();
                     dataInputStream.close();
                 } else {
+                    // Manda mensagem para outro peer que está se preparando para o download
                     dataOutputStream.writeUTF("INITIATING_DOWNLOAD");
 
                     String fileName = client.getRequestedFile();
 
+                    // Verifica se não há arquivo com mesmo nome na pasta
                     if (client.getFileNames().contains(fileName)) {
-                        Scanner sc = new Scanner(System.in);
-                        System.out.println("Ha um arquivo com mesmo nome na pasta, deseja substituir? [S/N]");
-                        String response = sc.nextLine();
-                        if (response.equals("S")) {
-                            File file = new File(
-                                    client.getFolderPath()
-                                            + File.separator
-                                            + fileName);
-                            file.delete();
-                        } else {
-                            int cont = 1;
-                            String baseName = client.getRequestedFile()
-                                    .substring(0, client.getRequestedFile().lastIndexOf('.'));
-                            String extension = client.getRequestedFile()
-                                    .substring(client.getRequestedFile().lastIndexOf('.') + 1);
-                            while (client.getFileNames().contains(baseName + "(" + cont + ")" + extension)) cont++;
-                            fileName = baseName + "(" + cont + ")" + extension;
-                        }
+
+                        // Altera nome de arquivo para que não ocorra erro
+                        int cont = 1;
+                        String baseName = client.getRequestedFile()
+                                .substring(0, client.getRequestedFile().lastIndexOf('.'));
+                        String extension = client.getRequestedFile()
+                                .substring(client.getRequestedFile().lastIndexOf('.') + 1);
+                        while (client.getFileNames().contains(baseName + "(" + cont + ")." + extension)) cont++;
+                        fileName = baseName + "(" + cont + ")" + extension;
 
                     }
 
+                    // Iniciando stream para escrever arquivo na pasta
                     FileOutputStream fileOutputStream = new FileOutputStream(
                             client.getFolderPath()
                                     + File.separator
                                     + fileName);
 
+                    // Cria buffer com tamanho dinâmico baseado no tamanho do buffer do socket
                     byte[] buffer = new byte[downloadBufferSize(socket.getReceiveBufferSize())];
                     int bytesRead;
+                    // Lê bytes enviados via TCP e escreve no arquivo
                     while ((bytesRead = dataInputStream.read(buffer)) != -1) {
                         fileOutputStream.write(buffer, 0, bytesRead);
+                        fileOutputStream.flush();
                     }
 
+                    // Mostra mensagem de confirmação do download
                     System.out.println("Arquivo "
                             + client.getRequestedFile()
                             + " baixado com sucesso na pasta "
                             + client.getFolderPath());
 
-                    client.updateFiles();
+                    // Envia requisição UPDATE via RMI
+                    String response = server.update(client.getIp(), client.getPort(), fileName);
 
-                    server.update(client.getIp(), client.getPort(), client.requestedFile);
+                    if (response.equals("UPDATE_OK")) {
+                        // Atualiza os arquivos do peer
+                        client.updateFiles();
+                    }
 
+                    // Fechando socket e streams
                     dataOutputStream.close();
                     dataInputStream.close();
                     fileOutputStream.close();
                     inputStream.close();
                     outputStream.close();
-                    socket.close();
                 }
-
+                socket.close();
 
 
             } catch (IOException e) {
@@ -272,6 +318,7 @@ public class PeerClient {
             }
         }
 
+        // Função para calcular tamanho do buffer
         private int downloadBufferSize(int socketBufferSize) {
             int bufferSize = socketBufferSize;
             if (bufferSize < BUFFER_SIZE) {
@@ -281,6 +328,7 @@ public class PeerClient {
         }
     }
 
+    // Thread que gerencia envio de arquivo a um outro peer
     static class DownloadSender implements Runnable {
         private static final int BUFFER_SIZE = 4096;
         private final Socket socket;
@@ -295,15 +343,18 @@ public class PeerClient {
         public void run() {
             try {
 
+                // Recebe nome de arquivo requisitado
                 InputStream inputStream = socket.getInputStream();
                 DataInputStream dataInputStream = new DataInputStream(inputStream);
                 String fileName = dataInputStream.readUTF();
 
+                // Inicia o outputStream para enviar informações via TCP
                 OutputStream outputStream = socket.getOutputStream();
                 DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
 
+                // Verifica se o arquivo requisitado está na pasta do peer
                 if (!client.getFileNames().contains(fileName)) {
-
+                    // Caso o arquivo não seja encontrado manda mensagem informando via TCP
                     dataOutputStream.writeUTF("FILE_NOT_FOUND");
 
                     dataInputStream.close();
@@ -311,20 +362,25 @@ public class PeerClient {
                     dataOutputStream.close();
                     outputStream.close();
                 } else {
-
+                    // Manda confirmação de que o arquivo foi encontrado
                     dataOutputStream.writeUTF("FILE_FOUND");
 
                     String confirmation = dataInputStream.readUTF();
 
+                    // Prepara arquivo para enviar
                     File file = new File(client.getFolderPath(), fileName);
                     FileInputStream fileInputStream = new FileInputStream(file);
 
+                    // Cria buffer com base no tamanho do arquivo
                     byte[] buffer = new byte[downloadBufferSize(file.length())];
                     int bytesRead;
+                    // Envia arquivo via TCP
                     while ((bytesRead = fileInputStream.read(buffer)) != -1) {
                         dataOutputStream.write(buffer, 0, bytesRead);
+                        dataOutputStream.flush();
                     }
 
+                    // Fecha socket e streams
                     dataInputStream.close();
                     inputStream.close();
                     fileInputStream.close();
@@ -337,6 +393,7 @@ public class PeerClient {
             }
         }
 
+        // Função para calcular tamanho do buffer
         private int downloadBufferSize(long fileSize) {
             int bufferSize = BUFFER_SIZE;
             if (fileSize > BUFFER_SIZE) {
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,6 +2,10 @@ import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
@@ -19,11 +23,13 @@ public class Server {
     }
 
     private static void serverInicialization() throws AlreadyBoundException, RemoteException {
+        // Captura IP do registro, caso não seja informado usa o valor padrão
         Scanner sc = new Scanner(System.in);
         System.out.print("Favor inserir o IP do registro:");
         String registryIP = sc.nextLine().trim();
         if (registryIP.isEmpty()) registryIP = DEFAULT_IP;
 
+        // Captura porta do registro, caso não seja informado usa o valor padrão
         System.out.print("Favor inserir a porta do registro:");
         String registryPort = sc.nextLine().trim();
         int registryPortNumber;
@@ -33,15 +39,83 @@ public class Server {
             registryPortNumber = parseInt(registryPort);
         }
 
-        serverRMI(registryIP, registryPortNumber);
+        // Chama função que inicia o RMI no lado do servidor
+        serverRMI(registryPortNumber);
     }
 
-    private static void serverRMI(String registryIP, int registryPort) throws RemoteException, AlreadyBoundException {
+    private static void serverRMI(int registryPort) throws RemoteException, AlreadyBoundException {
+        // Instancia classe com serviços que serão usados via RMI
         Requisitions requisitions = new Requisitions();
 
+        // Cria registro de RMI
         LocateRegistry.createRegistry(registryPort);
         Registry reg = LocateRegistry.getRegistry();
-        reg.rebind("server", requisitions);
+        // Faz rebind do serviço com o registro
+        reg.rebind("requisitions", requisitions);
+    }
+
+    public static class Requisitions extends UnicastRemoteObject implements RequisitionInterface {
+        // Estrutura em que as informações sobre os peers são guardadas
+        public HashMap<String, ArrayList<String>> joinedPeers = new HashMap<>();
+
+        protected Requisitions() throws RemoteException {
+        }
+
+        @Override
+        public String join(String ipPeer, int portPeer, ArrayList<String> files) throws RemoteException {
+            // Cria a chave do HashMap usando IP e porta
+            String peerKey = ipPeer + ":" + portPeer;
+
+            // Adiciona peer e lista de arquivos no HashMap
+            joinedPeers.put(peerKey, files);
+
+            // Mostra mensagem com informações do peer adicionado
+            System.out.println("Peer "
+                    + peerKey
+                    + " adicionado com arquivos "
+                    + String.join(" ", files));
+            return "JOIN_OK";
+        }
+
+        @Override
+        public ArrayList<String> search(String ipPeer, int portPeer, String fileName) throws RemoteException {
+            // Cria a chave do HashMap usando IP e porta
+            String peerKey = ipPeer + ":" + portPeer;
+
+            // Mostra mensagem informando peer e arquivo pedido
+            System.out.println("Peer " + peerKey + " solicitou arquivo " + fileName);
+
+            // Verifica e envia peers com arquivo solicitado
+            ArrayList<String> peersWithRequestedFile = new ArrayList<>();
+            for (Map.Entry<String, ArrayList<String>> entry : joinedPeers.entrySet()) {
+                String key = entry.getKey();
+                ArrayList<String> value = entry.getValue();
+                if (value.contains(fileName)) peersWithRequestedFile.add(key);
+            }
+            return peersWithRequestedFile;
+        }
+
+        @Override
+        public String update(String ipPeer, int portPeer, String fileName) throws RemoteException {
+            // Cria a chave do HashMap usando IP e porta
+            String peerKey = ipPeer + ":" + portPeer;
+
+            // Verifica se o peer já realizou o JOIN e se o arquivo não foi registrado e atualiza caso contrário
+            if (joinedPeers.containsKey(peerKey)) {
+                ArrayList<String> files = joinedPeers.get(peerKey);
+                if (!files.contains(fileName)) {
+                    files.add(fileName);
+                    joinedPeers.put(peerKey, files);
+                } else {
+                    return "Arquivo já está registrado";
+                }
+            } else {
+                return "Peer não encontrado";
+            }
+
+            return "UPDATE_OK";
+        }
+
     }
 
 
